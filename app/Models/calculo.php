a6d41f910a45ee03567e9ce3828411847b2d1ac2
Lucas Mateus Cruz Cardoso <lucas.cardoso@campus.unimontes.int>
2021-10-08 17:57:58 -0300

    Configuração do model, controller e resource, juntamente com a migration, se necessario para o mysql

---
+++ app/Models/calculo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class calculo extends Model
+{
+    use HasFactory;
+}
---
+++ database/factories/CalculoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\calculo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CalculoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = calculo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
