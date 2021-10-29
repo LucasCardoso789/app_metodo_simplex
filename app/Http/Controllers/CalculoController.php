71d0386c9ea36fc369b6f42de1c57d0e6cd8824a
Lucas Cardoso <lucasm.cardoso789@gmail.com>
2021-10-29 09:42:29 -0300

    Configuração na tela inicial do sistema

--- app/Http/Controllers/CalculoController.php
+++ app/Http/Controllers/CalculoController.php
@@ -14,6 +14,7 @@ class CalculoController extends Controller
      */
     public function index()
     {
+        
         return view('calculo');
     }
 
--- resources/views/calculo.blade.php
+++ resources/views/calculo.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Método Simplex</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
@@ -16,10 +16,19 @@
     <br>
     <div class="container">
 
-    <form action="problema.php" method="get" style="text-align:center;">
-        <p>Quantas variáveis?  <input id ="v" name="v" type="text" size="5"></p>
-        <p>Quantas restrições?  <input id ="r" name="r" type="text" size="5"></p>
-        <p><input class="btn btn-primary" type="submit" name="Submit" value="Continuar"></p>
+
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Quantas variáveis?</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">Os valores tem limite de 100</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Quantas restrições?</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="button" class="btn btn-success">Enviar</button>
+        </form>
     </form>
 
     </div>
