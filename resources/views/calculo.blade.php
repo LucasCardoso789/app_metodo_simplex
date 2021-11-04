6b5fc2528b3a9e712b9472a621ee9214508d86f0
GuilhermeSouza01 <ghostgui60@gmail.com>
2021-11-04 11:21:40 -0300

    Criando view da pagina inicial

--- resources/views/calculo.blade.php
+++ resources/views/calculo.blade.php
@@ -6,30 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Método Simplex</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
-<body>
+<body style="background-color: #ECFEAA">
     <br>
     <br>
     <br>
     <br>
     <br>
-    <div class="container">
+    <div class=" d-flex justify-content-center p-2 ">
+            <div class="d-flex justify-content-center bg-white rounded-3 border shadow-lg" >
+                <div class="row p-5  " >
+
+                    <form class="">
+                        <div class=" d-flex justify-content-center mb-9 pb-3">
+                            <div class="col-md-9">
+                                <label for="exampleInputEmail1" class="form-label">Número de variáveis:</label>
+                                <input type="number" class="form-control" id="variableNumber" placeholder="Digite um Número">
+                            <div id="emailHelp" class="form-text">Os valores tem limite de 100</div>
+                            </div>
+                        </div>
+
+                        <div class=" d-flex justify-content-center mb-9 pb-4">
+                            <div class="col-md-9">
+                                <label for="exampleInputEmail1" class="form-label">Número de Restrições:</label>
+                                <input type="number" class="form-control" id="variableNumber" placeholder="Digite um Número">
+                            </div>
+
+                        </div>
+
+                    </form>
+                    <div class="d-flex justify-content-center">
+                        <button type="button" class="btn btn-dark btn-lg text-light mb-9">Continuar</button>
+                    </div>
+                </div>
 
 
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Quantas variáveis?</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">Os valores tem limite de 100</div>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Quantas restrições?</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <button type="button" class="btn btn-success">Enviar</button>
-        </form>
-    </form>
 
     </div>
 </body>
